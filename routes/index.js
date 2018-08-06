53b2b8e354dc25ed2addeccc44d56560dd8da650
MyatThiri <myatthiriwinhtut@gmail.com>
2018-08-06 17:00:35 +0630

    Add /dupemail for signup

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,18 @@ router.post('/signup', function(req, res, next) {
   });
 });
 
+/* POST email duplication */
+router.post('/dupemail', function(req, res, next){
+  User.findByEmail(req.body.email, function(err, rows){
+    if (err) throw err;
+    if(rows.length > 0){
+      res.json({ status: true, msg:'Duplicated email!'});
+    }else{
+      res.json({ status: false})
+    }
+  });
+});
+
 /* GET signin page. */
 router.get('/signin', function(req, res, next) {
   res.render('commons/sign-in', { title: 'Signin' });
