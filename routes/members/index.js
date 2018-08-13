4dec92912e6b847fcd221c3d53ad30e0c53efee7
MyatThiri <myatthiriwinhtut@gmail.com>
2018-08-13 16:52:54 +0630

    Add Authorization

--- routes/members/index.js
+++ routes/members/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//members role check
+router.use(function(req,res,next){
+  if(req.session.user.role == 'USER'){
+    next();
+  }else{
+    req.flash('warn', 'Not allowed admin! Please login ');
+    res.redirect('/signin'); //redirect to other page
+  }
+});
 router.get('/', function(req,res){
   res.render('members/home', {title:'Members home'});
 });
