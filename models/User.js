d4e3714feedddf9b2b6db6333d04a6480a96305b
MyatThiri <myatthiriwinhtut@gmail.com>
2018-08-20 12:08:18 +0630

    Add search box

--- models/User.js
+++ models/User.js
@@ -3,15 +3,24 @@ var bcrypt = require('bcrypt-nodejs');
 
 var User = {
   add: function(params, callback){
-    var sql = 'INSERT INTO users (name, email, password,role) VALUES (?, ?, ?,?)';
+    var sql = "INSERT INTO users (name, email, password,role) VALUES (?, ?, ?,?)";
     params[2] = bcrypt.hashSync(params[2], bcrypt.genSaltSync(8), null);
     return db.query(sql, params, callback);
   },
   findByEmail: function(email, callback){
     console.log('email',email);
-    var sql = 'SELECT * FROM users WHERE email = ?';
+    var sql = "SELECT * FROM users WHERE email = ?";
     return db.query(sql, [email], callback);
   },
+
+  find: function(params, callback){
+    var sql = "SELECT uid,name,role, email,DATE_FORMAT(updated,'%d/%m/%Y %H:%i') AS updated FROM users";
+    if(params[0] != '')
+    sql += " WHERE name LIKE concat('%',?,'%') OR email LIKE concat('%',?,'%')";
+    console.log(sql);
+    return db.query(sql, params, callback);
+  },
+
   compare: function(cleartext, encrypted){
     return bcrypt.compareSync(cleartext, encrypted);
   }
--- routes/admin/users.js
+++ routes/admin/users.js
@@ -5,8 +5,13 @@ var router = express.Router();
 var User = require('../../models/User');
 
 
-router.get('/list', function(req, res, next) {
-  res.render('admin/users/user-list', {title: 'User list'});
-});
+router.all('/list', function(req, res, next) {
+  var params = [req.body.keyword ||'',req.body.keyword ||'' ];
+  User.find(params, function(err, users) {
+    console.log('users',users);
+    if (err) throw err;
+        res.render('admin/users/user-list', { title: 'User List', users: users, search:{keyword: req.body.keyword}});
+      });
+    });
 
 module.exports = router;
